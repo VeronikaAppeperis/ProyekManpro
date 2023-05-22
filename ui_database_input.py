cbd280abcc91cb6c71036d99deb5064495ad4cba
Brigitta Audrey <c14200188@john.petra.ac.id>
2023-05-22 14:18:43 +0700

    import data ke database

--- ui_database_input.py
+++ ui_database_input.py
@@ -1,6 +1,8 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QTableWidget, QTableWidgetItem, QHeaderView, QLabel, QLineEdit, QVBoxLayout
-from PyQt5.QtCore import Qt
+import sqlite3
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QTableWidget, QTableWidgetItem, QHeaderView, QLabel, QLineEdit, QVBoxLayout, QSpinBox
+
+app = QApplication(sys.argv)
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -55,33 +57,46 @@ class MainWindow(QMainWindow):
         layout.addWidget(label7)
         layout.addWidget(input7)
 
+        button = QPushButton('Submit', self)
+        layout.addWidget(button)
+
         widget = QWidget()
         widget.setLayout(layout)
 
         self.setCentralWidget(widget)
 
-        # Membuat button Enter
-        button = QPushButton('Enter', self)
-        button.clicked.connect(self.on_enter_clicked)
-
-        layout.addWidget(button)
+        button.clicked.connect(lambda: self.submitData(input1.text(), input2.text(), input3.text(), input4.text(), input5.text(), input6.text(), input7.text()))
 
         self.show()
 
-    def on_enter_clicked(self):
-        row_count = self.table.rowCount()
-        self.table.insertRow(row_count)
-        for column in range(self.table.columnCount()):
-            item = QTableWidgetItem()
-            item.setTextAlignment(Qt.AlignCenter)
-            item.setText(self.layout().itemAt(column * 2 + 1).widget().text())
-            self.table.setItem(row_count, column, item)
+    def submitData(self, name, category, funding, country, state, region, city):
+        # buat koneksi ke database
+        conn = sqlite3.connect('startup.db')
+        cursor = conn.cursor()
 
-        # Menghapus teks input setelah tombol Enter ditekan
-        for i in range(self.layout().count()):
-            if isinstance(self.layout().itemAt(i).widget(), QLineEdit):
-                self.layout().itemAt(i).widget().clear()
+        # buat tabel jika belum ada
+        cursor.execute('''CREATE TABLE IF NOT EXISTS startUp
+                          (name TEXT, category TEXT, funding INT, country TEXT, state TEXT, region TEXT, city TEXT)''')
 
-app = QApplication(sys.argv)
-window = MainWindow()
-sys.exit(app.exec_())
+        # masukkan data ke dalam tabel
+        cursor.execute("INSERT INTO startUp VALUES (?, ?, ?, ?, ?, ?, ?)", (name, category, int(funding), country, state, region, city))
+        conn.commit()
+
+        # tampilkan data di tabel
+        rowCount = self.table.rowCount()
+        self.table.insertRow(rowCount)
+        self.table.setItem(rowCount, 0, QTableWidgetItem(name))
+        self.table.setItem(rowCount, 1, QTableWidgetItem(category))
+        self.table.setItem(rowCount, 2, QTableWidgetItem(str(funding)))
+        self.table.setItem(rowCount, 3, QTableWidgetItem(country))
+        self.table.setItem(rowCount, 4, QTableWidgetItem(state))
+        self.table.setItem(rowCount, 5, QTableWidgetItem(region))
+        self.table.setItem(rowCount, 6, QTableWidgetItem(city))
+
+        # Menutup koneksi ke database
+        cursor.close()
+        conn.close()
+
+if __name__ == '__main__':
+    window = MainWindow()
+    sys.exit(app.exec_())
